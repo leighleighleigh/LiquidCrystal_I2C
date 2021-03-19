6f31f6130d60d6cbd34e2b22378e535933d64c50
Leigh <leighcharlesoliver@gmail.com>
2021-03-19 15:08:10 +1100

    Moved setCursor to AC input

--- LiquidCrystal_I2C.h
+++ LiquidCrystal_I2C.h
@@ -14,7 +14,7 @@
 #define LCD_CURSORSHIFT 0x10
 #define LCD_FUNCTIONSET 0x20
 #define LCD_SETCGRAMADDR 0x40
-#define LCD_SETDDRAMADDR 0x80
+#define LCD_SETDDRAMADDR 0x80 // 10000000 | 1 = 10000001
 
 // flags for display entry mode
 #define LCD_ENTRYRIGHT 0x00
@@ -80,7 +80,7 @@ public:
   void createChar(uint8_t location, const char *charmap);
   // Example: 	const char bell[8] PROGMEM = {B00100,B01110,B01110,B01110,B11111,B00000,B00100,B00000};
   
-  void setCursor(uint8_t, uint8_t); 
+  void setCursor(uint8_t); 
 #if defined(ARDUINO) && ARDUINO >= 100
   virtual size_t write(uint8_t);
 #else
